72bd9cf4841ffd71c8c23d3e245c74ef1bcf0619
njmhrzn <136047244+njmhrzn@users.noreply.github.com>
2023-06-13 06:30:15 +0800

    Update streamlit_app.py
    Added fruit shake header

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
